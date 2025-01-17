d027ae12d7a2e005af89d0eb06e1e83d1d6601f0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-16 22:55:50 -0600

    currency name rendered in the form

--- cripto-coin/src/components/CriptoSearchForm.tsx
+++ cripto-coin/src/components/CriptoSearchForm.tsx
@@ -1,10 +1,17 @@
+import { currencies } from "../data";
+
 export default function CriptoSearchForm() {
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Currency: </label>
         <select name="currency" id="currency">
-          <option value="">-- Select --</option>
+          <option value="currency">-- Select --</option>
+          {currencies.map((currency) => (
+            <option key={currency.code} value={currency.code}>
+              {currency.name}
+            </option>
+          ))}
         </select>
       </div>
 
