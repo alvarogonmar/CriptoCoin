d57337fd79b73fd335435a43441a5b7031cf50da
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-17 22:54:18 -0600

    adding has result function

--- cripto-coin/src/components/CryptoPriceDisplay.tsx
+++ cripto-coin/src/components/CryptoPriceDisplay.tsx
@@ -1,18 +1,24 @@
+import { useMemo } from "react";
 import { useCryptoStore } from "../store";
 
 export default function CryptoPriceDisplay() {
   const result = useCryptoStore((state) => state.result);
+  const hasResult = useMemo(() => Object.keys(result).length > 0, [result]);
 
   return (
     <div>
-      <h2>Price Tracker</h2>
-      <div className="result">
-        <div>
-          <p>
-            Price: <span>{result.PRICE}</span>
-          </p>
-        </div>
-      </div>
+      {hasResult && (
+        <>
+          <h2>Price Tracker</h2>
+          <div className="result">
+            <div>
+              <p>
+                Price: <span>{result.PRICE}</span>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
