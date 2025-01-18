f2c737f7de4a48a74a3b948e5f2547f12816f09d
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-17 22:43:23 -0600

    showing result price

--- cripto-coin/src/components/CryptoPriceDisplay.tsx
+++ cripto-coin/src/components/CryptoPriceDisplay.tsx
@@ -1,3 +1,18 @@
+import { useCryptoStore } from "../store";
+
 export default function CryptoPriceDisplay() {
-  return <div>CryptoPriceDisplay</div>;
+  const result = useCryptoStore((state) => state.result);
+
+  return (
+    <div>
+      <h2>Price Tracker</h2>
+      <div className="result">
+        <div>
+          <p>
+            Price: <span>{result.PRICE}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
